fa522f1cbcfc2975c82a85d0f5422937c913ec1f
Ihab <Ihab@users-MacBook-Pro.local>
2017-05-19 11:56:46 -0400

    jawdb update

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 var mysql = require("mysql");
 
-var connection;
 
 var JAWSDB_URL = {
     port: 3306,
@@ -10,17 +9,7 @@ var JAWSDB_URL = {
     database: "p6tilh0efzx9j5zo"
 }
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "burgers_db"
-    });
-};
-
+var connection = mysql.createConnection(JAWSDB_URL);
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
