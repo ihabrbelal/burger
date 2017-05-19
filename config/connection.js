85da08a5acd00b6f8e7770420b2718246b1e0520
Ihab <Ihab@users-MacBook-Pro.local>
2017-05-19 12:08:19 -0400

    jawdb update

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 var mysql = require("mysql");
 
+var connection;
 
-var JAWSDB_URL = {
-    port: 3306,
-    host: "vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "zhjfgifgk712xaou",
-    password: "n6s87py8ihhaa5gt",
-    database: "p6tilh0efzx9j5zo"
-}
+var JAWSDB_URL = "mysql://zhjfgifgk712xaou:p6tilh0efzx9j5zo@vvfv20el7sb2enn3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hg7anv85qr3gmo6u"
 
-var connection = mysql.createConnection(JAWSDB_URL);
+
+
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
+
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
